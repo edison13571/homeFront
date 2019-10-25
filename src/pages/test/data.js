199a052672c9d14fdb3ac309d2cc2b43d4b6e615
edison13571 <740336406@qq.com>
2019-10-25 16:45:05 +0800

    test try1

---
+++ src/pages/test/data.js
@@ -0,0 +1,8 @@
+export let dataArr=[ { id: 1, answer: 'It seems like', title: '看起来怎么样，好像怎么样', theme: '口语' },
+  { id: 2, answer: 'It will do', title: '行，可以', theme: '口语' },
+  { id: 3, answer: 'just because', title: '只是因为', theme: '口语' },
+  { id: 4, answer: 'It\'s supposed to', title: '“应该怎么样”相当于“should”', theme: '口语' },
+  { id: 5, answer: 'It\'s up to...', title: '有什么什么决定；取决于什么', theme: '口语' },
+  { id: 6, answer: 'It\'s worthy of', title: '它是值得的', theme: '口语' } ];
+
+
---
+++ src/pages/test/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="main">
+      <div v-for="(item,index) in list" :key="index">
+        <div class="action-button">
+          <div>听</div>
+          <div>看题目</div>
+          <div>答案</div>
+        </div>
+        <!--<div class="test">123</div>-->
+
+        <!--<div @click="seeAnswer(item.id)">{{item.title}}</div>-->
+        <!--<div v-show="item.see">{{item.answer}}</div>-->
+        <!--<div v-show="checkRight(item.answer)">yes</div>-->
+      </div>
+      <input v-model="str"/>
+
+  </div>
+</template>
+
+<script>
+  import {dataArr} from "./data"
+  export default {
+    name: 'Main',
+    components: {},
+    data() {
+      return {
+        list:[],
+        str:""
+      }
+    },
+    created() {
+      this.list=dataArr
+      // console.log(dataArr)
+    },
+    methods: {
+      checkRight(str){
+        return str===this.str
+      },
+      seeAnswer(id){
+        this.list=this.list.map(item=>{
+          if(item.id===id){
+            item.see=!item.see;
+          }
+          return item
+        })
+      }
+
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .test{
+    /*background: white;*/
+    /*color: transparent;*/
+  }
+  .main {
+    text-align: center;
+    padding-top: 80px;
+  }
+  .action-button{
+    margin: 0 auto;
+    width: 200px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+</style>
